f8a6669d8efe9d6f16e5283282e71d4c716d71e6
shvmsh20 <itsshivamsharma21@gmail.com>
2023-02-28 18:17:09 +0530

    add: component folder

---
+++ src/components/CompDidMountAxios.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import Post from './Post';
+import Comment from './Comment';
+import User from "./User";
+
+class CompDidMountAxios extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             renderType: "posts",
+             items: []
+        }
+
+    }
+
+    componentDidMount(){
+        axios.get(`https://jsonplaceholder.typicode.com/posts`)
+            .then(res => this.setState({
+                items: res.data
+            }));
+    }
+
+    delRow = (id)=>{
+        let  newItems = this.state.items.filter(item=>item.id!==id);
+        this.setState({
+            items: newItems
+        })
+        
+    }
+
+    compareBy = (key) => {
+        return function(a, b) {
+        if (a[key] < b[key]) return -1;
+        if (a[key] > b[key]) return 1;
+        return 0;
+        };
+      };
+
+    sortBy = (key) => {
+    let arrayCopy = [...this.state.items];
+    arrayCopy.sort(this.compareBy(key));
+    this.setState({items: arrayCopy});
+    };
+    
+    handleUpdate = (renderValue)=>{
+        if(this.state.renderType!==renderValue){
+            axios.get(`https://jsonplaceholder.typicode.com/${renderValue}`)
+            .then(res => this.setState({
+                renderType: renderValue,
+                items: res.data
+            }));
+            
+        }
+    }
+
+  render() {
+    return (
+      <div>
+        <button className='btn' onClick={()=>this.handleUpdate("posts")}>POSTS</button>
+        <button className='btn' onClick={()=>this.handleUpdate("comments")}>COMMENTS</button>
+        <button className='btn' onClick={()=>this.handleUpdate("users")}>USERS</button>
+      
+        <h1 className='heading'>{(this.state.renderType).toUpperCase()}</h1>
+
+        {this.state.renderType==="posts" && <Post items = {this.state.items} del={this.delRow} sorting={this.sortBy}/>}
+        {this.state.renderType==="comments" && <Comment items = {this.state.items} del={this.delRow} sorting={this.sortBy}/>}
+        {this.state.renderType==="users" && <User items = {this.state.items} del={this.delRow} sorting={this.sortBy}/>}
+        
+        
+    </div>
+    )
+  }
+}
+
+export default CompDidMountAxios
---
+++ src/components/User.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import DeleteIcon from '@mui/icons-material/Delete';
+import ImportExportIcon from '@mui/icons-material/ImportExport';
+
+function Comment({items, del, sorting}) {
+    return <table>
+    <thead>
+    <tr>
+    <th><div className='row'><span className='col'>ID</span>
+    <ImportExportIcon className='col sort-icon' onClick={()=>sorting('id')}/></div></th>
+
+    <th><div className='row'><span className='col'>Name</span>
+    <ImportExportIcon className='col sort-icon' onClick={()=>sorting('name')}/></div></th>
+
+    <th><div className='row'><span className='col'>Username</span>
+    <ImportExportIcon className='col sort-icon' onClick={()=>sorting('username')}/></div></th>
+
+    <th><div className='row'><span className='col'>Email</span>
+    <ImportExportIcon className='col sort-icon' onClick={()=>sorting('email')}/></div></th>
+
+    <th><div className='row'><span className='col'>Address</span>
+    <ImportExportIcon className='col sort-icon' onClick={()=>sorting('address[street')}/></div></th>
+
+    <th><div className='row'><span className='col'>Phone</span>
+    <ImportExportIcon className='col sort-icon' onClick={()=>sorting('phone')}/></div></th>
+
+    <th><div className='row'><span className='col'>Website</span>
+    <ImportExportIcon className='col sort-icon' onClick={()=>sorting('website')}/></div></th>
+
+    <th>Delete</th>
+    
+    </tr>
+    </thead>
+    <tbody>
+    {items && items.map((item)=>
+       (<tr key = {item.id}><td>{item.id}</td><td>{item.name}</td><td>{item.username}</td>
+      <td>{item.email}</td>
+      <td>{item.address["street"]}, {item.address["suite"]}, {item.address["city"]} , {item.address["zipcode"]} </td>
+      <td>{item.phone}</td><td>{item.website}</td><td><DeleteIcon className='del-btn' onClick={()=>del(item.id)}/></td>
+      </tr>)
+     )}
+     </tbody>
+     </table>
+
+
+}
+export default Comment
